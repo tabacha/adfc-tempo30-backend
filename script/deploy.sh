96322d24a02d21152fbf188734f6353e5cc7de2b
Sven Anders <sven@anders.hamburg>
2016-09-11 11:41:20 +0200

    fix deploy url

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -r --delete-after --exclude=travis-ssh-key   --quiet $TRAVIS_BUILD_DIR/ travis@tools.adfc-hamburg.de:/var/www/html/tempo30b/$TRAVIS_BRANCH
+rsync -r --delete-after --exclude=travis-ssh-key   --quiet $TRAVIS_BUILD_DIR/ travis@tools.adfc-hamburg.de:/var/www/html/tempo30-backend/$TRAVIS_BRANCH
