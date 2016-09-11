a4b37b2ac0aac17247ed8f0ed76c572560c3a0be
Sven Anders <sven@anders-hamburg.de>
2016-09-11 11:18:05 +0200

    fix rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ travis@tools.adfc-hamburg.de:/var/www/html/tempo30b/$TRAVIS_BRANCH
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travis@tools.adfc-hamburg.de:/var/www/html/tempo30b/$TRAVIS_BRANCH
