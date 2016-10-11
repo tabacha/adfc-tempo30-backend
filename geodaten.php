9c69212f356b855bf90f697950758c40b8e45617
Sven Anders <sven@anders.hamburg>
2016-10-11 09:34:11 +0200

    cleanup backend fix

--- geodaten.php
+++ geodaten.php
@@ -12,6 +12,7 @@ function distanceExpr($geom1, $geom2) {
 function queryMaxValue($attr, $table, $geom, $point, $radius) {
    return 'SELECT MAX('. $attr.') AS "'.$attr.'" FROM '.$table.' WHERE ST_Distance(ST_Transform('.$point.', 3857) , ST_Transform('.$geom.', 3857)) < '.$radius;
 }
+
 $lat = filter_input(INPUT_GET, 'lat',FILTER_VALIDATE_FLOAT);
 $lon = filter_input(INPUT_GET, 'lon',FILTER_VALIDATE_FLOAT);
 
@@ -24,59 +25,32 @@ $point= 'ST_GeometryFromText( \'POINT ( '.$lon.' '.$lat.' )\', 4326)';
 $con=pg_connect("dbname=gis") or print("cant connect");
 
 $rtn=array();
-$query = 'SELECT bemerkung,  region,  pk, vd , p.polizeirev, name, strasse, plz, ort, tel FROM PKGrenzen g, polizei p WHERE p.polizeirev=g.polizeirev and ST_Within('.$point.', wkb_geometry);';
-$result = pg_query($con, $query);
 
-if (!$result)  {
-                  echo "An error occured.\n";
-		  exit;
+function queryToArray($section, $query ) {
+    global $rtn, $con;
+    $result = pg_query($con, $query);
+    if (!$result)  {
+	 header("HTTP/1.0 500 Internal Server Error on query ".$section);
+         die("queryToArray". $query. $section);
+    } 
+    $rtn[$section]= pg_fetch_all($result, NULL, PGSQL_ASSOC);
 }
-$rtn['polizei']= pg_fetch_all($result, NULL, PGSQL_ASSOC);
+
+$query = 'SELECT bemerkung,  region,  pk, vd , p.polizeirev, name, strasse, plz, ort, tel FROM PKGrenzen g, polizei p WHERE p.polizeirev=g.polizeirev and ST_Within('.$point.', wkb_geometry);';
+queryToArray('polizei', $query);
 
 $query = 'SELECT bezirk_name, stadtteil, ortsteilnummer, bezirk FROM verwaltungsgrenzen WHERE ST_Within('. $point .', wkb_geometry);';
-$result = pg_query($con, $query);
-
-
-
-if (!$result)  {
-                  echo "An error occured.\n";
-		  exit;
-}
-
-$rtn['ort']= pg_fetch_all($result, NULL, PGSQL_ASSOC);
-
+queryToArray('ort', $query);
 
 $query = queryMaxValue('klasse','laerm_tag', $point , 'wkb_geometry',25);
-$result = pg_query($con, $query);
-
-if (!$result)  {
-                  echo "An error occured.\n";
-		  exit;
-}
-
-$rtn['laerm_tag']= pg_fetch_all($result, NULL, PGSQL_ASSOC);
+queryToArray('laerm_tag', $query);
 
 $query = queryMaxValue('klasse','laerm_nacht', $point , 'wkb_geometry',25);
-$result = pg_query($con, $query);
-
-if (!$result)  {
-                  echo "An error occured.\n";
-		  exit;
-}
-$rtn['laerm_nacht']= pg_fetch_all($result, NULL, PGSQL_ASSOC);
+queryToArray('laerm_nacht', $query);
 
 $radius = 500;
-
 $query = 'SELECT gid, name_12, no2_i1_gb, pm10_i1_gb, pm25_i1_gb,ST_AsText( geom), '.distanceExpr($point,'geom').' FROM luftdaten2015 WHERE ' . radiusQuery('geom',$point, $radius). ' AND ((no2_i1_gb!=0) OR (pm10_i1_gb!=0) OR (pm25_i1_gb!=0)) ORDER BY st_distance;';
-
-$result = pg_query($con, $query);
-
-if (!$result)  {
-                  echo "An error occured.\n";
-		  exit;
-}
-
-$rtn['luftdaten']= pg_fetch_all($result, NULL, PGSQL_ASSOC);
+queryToArray('luftdaten', $query);
 
 print  json_encode($rtn);
 
