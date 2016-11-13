27ff66627857b5082d124da0e4132abc289440b2
Sven Anders <sven@anders.hamburg>
2016-11-13 13:11:49 +0100

    add linefeed in logfile

--- track.php
+++ track.php
@@ -1,4 +1,4 @@
 <?php
-$log=sprintf("%d %s",time(),$_SERVER['QUERY_STRING']);
+$log=sprintf("%d %s\n",time(),$_SERVER['QUERY_STRING']);
 echo $log;
 file_put_contents('/var/log/tempo30/track_'.date("Y-n-j").'.log', $log, FILE_APPEND);
