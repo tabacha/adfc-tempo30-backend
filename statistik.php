fe65d0a853af9cc23aec044f2dfc3b648a4623ae
Manfred Meyer <manfred-meyer@posteo.de>
2017-01-29 10:56:29 +0100

    More Statistik

--- statistik.php
+++ statistik.php
@@ -1,20 +1,55 @@
 <?php
-print "{validated:";
+
+$jsonresult = array();
 $con=pg_connect("dbname=gis") or print("cant connect");
 
 $result = pg_query($con, "SELECT COUNT(*) from antraege WHERE mailchecked=true;");
 $row = pg_fetch_row($result);
 
-print $row[0];
+$jsonresult['validated_by_email']=$row[0];
 
-print ",no_validated:";
-
-$result = pg_query($con, "SELECT COUNT(*) from antraege WHERE mailchecked=false;");
+$result = pg_query($con, "SELECT COUNT(*) from antraege WHERE adrchecked=true;");
 $row = pg_fetch_row($result);
 
-print $row[0];
-print "}";
+$jsonresult['validated_by_adr']=$row[0];
 
+
+$result = pg_query($con, "SELECT COUNT(*) from antraege WHERE mailchecked=false and adrchecked=false;");
+$row = pg_fetch_row($result);
+
+$jsonresult['no_validated']=$row[0];
+
+$result = pg_query($con, "SELECT bezirk, COUNT(*)  from antraege where mailchecked=true group by bezirk;");
+
+$resultArray = pg_fetch_all($result);
+$jsonresult['validated_by_bezirk']=$resultArray;
+
+$result = pg_query($con, "SELECT bezirk, COUNT(*)  from antraege where mailchecked=true and showinmap=true group by bezirk;");
+
+$resultArray = pg_fetch_all($result);
+$jsonresult['in_map_by_bezirk']=$resultArray;
+
+$result = pg_query($con, "SELECT bezirk, COUNT(*)  from antraege where mailchecked=true and saveanschrift=true group by bezirk;");
+
+$resultArray = pg_fetch_all($result);
+$jsonresult['with_adr_by_bezirk']=$resultArray;
+
+$result = pg_query($con, "SELECT bezirk, COUNT(*)  from antraege where mailchecked=true and newsletter=true group by bezirk;");
+
+$resultArray = pg_fetch_all($result);
+$jsonresult['newsletter_by_bezirk']=$resultArray;
+
+
+$result = pg_query($con, "select lastchanged from antraege order by lastchanged desc limit 1;");
+$row = pg_fetch_row($result);
+
+$jsonresult['last_db_changed']=$row[0];
+
+
+$jsonresult['timestamp']=date('Y-m-d H:i:s');
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($jsonresult,JSON_PRETTY_PRINT| JSON_NUMERIC_CHECK| JSON_UNESCAPED_UNICODE);
 
 pg_close($con);
 
+exit;
